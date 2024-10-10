9f461149c8749ac4c48ad5348d96d6ef7be41273
glorious0119 <orimoloyekayode@outlook.com>
2024-10-10 19:11:44 -0400

    Update engine.py

--- Part_3_pet_modules/engine.py
+++ Part_3_pet_modules/engine.py
@@ -19,11 +19,7 @@ def train_step(model: torch.nn.Module,
   train_loss, train_acc = 0, 0
 
   # Loop through data loader data batches
-  for batch, data in enumerate(dataloader):
-
-    print(f"Batch {batch}: data = {data}")  # Inspect the output
-
-    (X, y) = data
+  for batch, (X, y) in enumerate(dataloader):
 
     # Send data to target device
     X, y = X.to(device), y.to(device)
